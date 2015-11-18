f65ddbbb869230515c4ac6e2078da5f35955f4a9
Terry Zhang <zhangqingyu@loukou.com>
2015-11-18 09:51:02 +0800

    [update] public headers

--- QYCommonKit/QYCommonKit.h
+++ QYCommonKit/QYCommonKit.h
@@ -9,5 +9,15 @@
 #ifndef QYCommonKit_h
 #define QYCommonKit_h
 
+#import "NSString+MD5.h"
+#import "NSString+Sha1.h"
+#import "NSArray+QYSafe.h"
+#import "NSMutableArray+QYSafe.h"
+#import "NSString+ValidateCheck.h"
+#import "UIView+QYFrame.h"
+#import "UIView+QYAutoLayout.h"
+#import "UIColor+QYHex.h"
+#import "QYAppInfo.h"
+
 
 #endif /* QYCommonKit_h */
